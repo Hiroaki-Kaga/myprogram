7791fa508e612bd033731fdc6cac814281b30d9f
EC2 Default User <ec2-user@ip-172-31-17-216.us-east-2.compute.internal>
2020-03-31 13:41:11 +0000

    Laravel08

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.pfofile.create');
+    }
+    
+    public function create()
+    { 
+        return redirect('admin/profile/crete');
+    }
+    
+    public function edit()
+    {
+        return view('admin/profile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+    
+}
+
